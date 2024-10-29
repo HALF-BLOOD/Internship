5b2e5d17b0e172024a5f59629b7c6d4eb9256707
HALF-BLOOD <92146650+HALF-BLOOD@users.noreply.github.com>
2024-10-29 15:38:25 +0545

    Update books.sql

--- Book store/books.sql
+++ Book store/books.sql
@@ -11,8 +11,28 @@ INSERT INTO books (ID, title, publish_date, description) VALUES
 (1, 'Book A', '2023-05-20', 'Description for Book A'),
 (2, 'Book B', '2022-03-15', 'Description for Book B');
 
+INSERT INTO books (ID,title, genre_id, publish_date, description, created_at, updated_at)
+VALUES (3, 'Sample Book', 1, '2023-01-01', 'A sample book description.', NOW(), NOW());
+
+INSERT INTO books (ID, title, author_id, publish_date, description, created_at, updated_at)
+VALUES (4, 'Sample Book', 1, '2023-01-01', 'A test book description.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 ALTER TABLE books
 ADD author_id INT,
 ADD genre_id INT,
 ADD FOREIGN KEY (author_id) REFERENCES authors(ID),
 ADD FOREIGN KEY (genre_id) REFERENCES genres(ID);
+
+CREATE VIEW BookStockView AS
+SELECT 
+    books.ID,
+    books.title AS book_title,
+    inventory.available_copies AS stock_level,
+    inventory.last_restock_date
+FROM 
+    books
+JOIN 
+    inventory ON books.ID = inventory.book_id;
+SELECT * FROM BookStockView;
+
+
