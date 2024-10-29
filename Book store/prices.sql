ec85a0a73166d5cb565d15c2d78593e1646e484d
HALF-BLOOD <92146650+HALF-BLOOD@users.noreply.github.com>
2024-10-29 15:41:49 +0545

    Update prices.sql

--- Book store/prices.sql
+++ Book store/prices.sql
@@ -14,3 +14,26 @@ CREATE TABLE prices (
 INSERT INTO prices (price_id, book_id, price, discount_price, effective_date, expiration_date) VALUES
 (1, 1, 20.00, 15.00, '2023-01-01', '2023-12-31'),
 (2, 2, 25.00, 20.00, '2023-01-01', '2023-12-31');
+
+INSERT INTO prices (price_id, book_id, price, discount_price, effective_date, expiration_date, created_at, updated_at)
+VALUES (4, 1, 100.00, 80.00, '2023-01-01', '2025-01-01', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+
+CREATE VIEW Current_Day_Book_Pricing AS
+SELECT 
+    b.ID AS book_id,
+    b.title AS book_title,
+    p.price AS current_price,
+    p.discount_price AS current_discount_price,
+    p.effective_date,
+    p.expiration_date
+FROM 
+    books b
+JOIN 
+    prices p ON b.ID = p.book_id
+WHERE 
+    CURRENT_DATE BETWEEN p.effective_date AND p.expiration_date
+ORDER BY 
+    b.title;
+
+Select * from Current_Day_Book_Pricing;
