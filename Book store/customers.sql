7013114c424d477ab0a22b7cb3a3fad5e3b15256
HALF-BLOOD <92146650+HALF-BLOOD@users.noreply.github.com>
2024-10-29 15:38:52 +0545

    Update customers.sql

--- Book store/customers.sql
+++ Book store/customers.sql
@@ -13,3 +13,35 @@ CREATE TABLE customers (
 INSERT INTO customers (ID, customer_name, email, phone, primary_address, billing_address) VALUES
 (1, 'Customer A', 'customerA@example.com', '1231231234', '123 Primary St', '123 Billing St'),
 (2, 'Customer B', 'customerB@example.com', '9879879876', '456 Primary Ave', '456 Billing Ave');
+
+DROP view Customer_Order_Discount_HIstory;
+CREATE VIEW Customer_Order_Discount_History AS
+SELECT 
+    c.ID AS customer_id,
+    c.customer_name,
+    c.phone,
+    o.ID AS order_id,
+    o.total_amount AS order_total_amount,
+    p.payment_status,
+    oi.ID AS order_item_id,
+    oi.price_at_sale,
+    oi.quantity,
+    oi.discount_applied AS order_item_discount,
+    d.discount_type
+FROM 
+    customers c
+JOIN 
+    orders o ON c.ID = o.customer_id
+LEFT JOIN 
+    payments p ON o.ID = p.order_id
+JOIN 
+    order_items oi ON o.ID = oi.order_id
+LEFT JOIN 
+    discount_applications da ON oi.ID = da.order_item_id
+LEFT JOIN 
+    discounts d ON da.discount_id = d.ID
+ORDER BY 
+    c.ID, o.order_date;
+
+
+select * from Customer_Order_Discount_History;
